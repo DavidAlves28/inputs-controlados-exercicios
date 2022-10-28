9051dcf5df3c07363979ceebaff296fc4866e6a7
DavidAlves28 <dav.alvesc28@gmail.com>
2022-10-27 21:23:47 -0300

    Resolucao-inputs

--- src/Components/ConfirmationForm/ConfirmationForm.js
+++ src/Components/ConfirmationForm/ConfirmationForm.js
@@ -1,12 +1,111 @@
-import React from 'react'
-import { Form} from '../MainPage/styles'
+import React, { useState } from 'react'
+import MainPage from '../MainPage/MainPage'
+import { Form, Input } from '../MainPage/styles'
+import NameForm from '../NameForm/NameForm'
 
-const ConfirmationForm = () => {
+
+const ConfirmationForm = (props) => {
+  const [dataNas, setDataNas] = useState("")
+  const changedata = (event) => {
+    setDataNas(event.target.value)
+  }
+
+  const [fone, setFone] = useState("")
+  const changefone = (event) => {
+    setFone(event.target.value)
+  }
+
+  const [genero, setGenero] = useState("");
+  const changeGenero = (event) => {
+    setGenero(event.target.value)
+  }
+  const [raca, setRaca] = useState("")
+  const changeRaca = (e) => {
+    setRaca(e.target.value)
+  }
+  const [curso, setCurso] = useState("")
+  const changeCurso = (e) => {
+    setCurso(e.target.value)
+  }
+  const [ensino, setEnsino] = useState("")
+  const changeEnsino = (e) => {
+    setEnsino(1)
+  }
+  const [disponivel, setDisponivel] = useState("")
+  const changeDisponivel = (e) => {
+    setDisponivel(1)
+  }
+  // console.log(dataNas);
+  // console.log(fone);
+  // console.log(genero)
+  // console.log(cor)
+  // console.log(curso);
+  // console.log(ensino);
+  // console.log(disponivel);
+  const enviarDados = () => {
+    if (dataNas.length !== 0 && fone.length > 0 && genero.length > 0 && raca.length > 0 && ensino === 1 && disponivel === 1) {
+      if (curso === "noturno" || curso === "integral") {
+        setGenero("")
+        setRaca("")
+        setDataNas("")
+        setFone("")
+        alert("Prontinho! Seus dados foram enviados");
+        props.form2(1)
+      }
+    } else
+      alert("Preencha todos campos!")
+    setGenero("")
+    setRaca("")
+    setDataNas("")
+    setFone("")
+
+  }
   return (
     <Form>
-        
-      <button>Enviar dados</button>
-      </Form>
+      <label>Data de Nascimento :
+        <Input type="date" value={dataNas} onChange={changedata} />
+      </label>
+      <label>Telefone:
+        <Input type="text" value={fone} onChange={changefone} />
+      </label>
+      <label> Escolha seu Gênero :
+        <select value={genero} onChange={changeGenero}>
+          <option ></option>
+          <option >Homem-cisgênero</option>
+          <option >Mulher-cisgênero</option>
+          <option >Homem-Trans</option>
+          <option >Mulher-Trans</option>
+          <option >Pessoa não binária</option>
+          <option >Prefiro não dizer</option>
+        </select>
+      </label>
+      <label> Você se considera :
+        <select value={raca} onChange={changeRaca}>
+          <option ></option>
+          <option >Negro</option>
+          <option >Branco</option>
+          <option >Pardo</option>
+          <option >Amarelo</option>
+          <option >Indígina</option>
+          <option >Prefiro não dizer</option>
+        </select>
+      </label>
+      <form value={curso} onChange={changeCurso}> Escolha seu curso :  <br></br>
+        <input type="radio" id="integral" name='curso' value="integral" />
+        <label for="integral"> Curso integral web fullstack</label><br></br>
+        <input type="radio" id="noturno" name='curso' value="noturno" />
+        <label for="noturno"> Curso noturno web fullstack</label>
+      </form>
+      <form value={ensino} onChange={changeEnsino} >
+        <p>Selecione :</p>
+        <input type="checkbox" id="ensinoMedio" name='ensinoMedio' value={ensino} onChange={changeEnsino} />
+        <label for="ensinoMedio"> Ensino Médio completo.</label><br></br>
+        <input type="checkbox" id="disponivel" name='disponivel' value={disponivel} onChange={changeDisponivel} />
+        <label for="disponivel"> Disponibilidade para participar das atividades.</label>
+
+      </form>
+      <button onClick={enviarDados}>Enviar dados</button>
+    </Form>
   )
 }
 
--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,37 +1,65 @@
-import React, {useState} from 'react'
-import {MainContainer} from './styles'
+import React, { useState } from 'react'
+import { MainContainer } from './styles'
 import NameForm from '../NameForm/NameForm'
 import ConfirmationForm from '../ConfirmationForm/ConfirmationForm'
 
 const MainPage = () => {
-const [name, setName] = useState("")
-const [age, setAge] = useState("")
-const [email, setEmail] = useState("")
-const [formFlow, setFormFlow] = useState(1) //você não precisa mexer neste estado, ele faz parte da lógica da linha 30 do JSX
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+  const [checarEmail, setChecarEmail] = useState("");;//exercicio
+  const [formFlow, setFormFlow] = useState(1) //você não precisa mexer neste estado, ele faz parte da lógica da linha 30 do JSX
 
-const onChangeName = (event) => {
-  setName(event.target.value)
-}
+  const onChangeName = (event) => {
+    setName(event.target.value)
+  }
 
-const onChangeAge = (event) => {
-  setAge(event.target.value)
-}
+  const onChangeAge = (event) => {
+    setAge(event.target.value)
+  }
 
-const onChangeEmail = (event) => {
-  setEmail(event.target.value)
-}
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value)
+  }
+  // exercicio 2 
+  const onChangeEmailVerifica = (event) => {
+    setChecarEmail(event.target.value)
+  }
 
-const sendData = () => {
-  //aqui deve vir uma verificação para mudar de formulario apenas se todos os requisitos tiverem sido cumpridos
-  setFormFlow(2)
-}
+  // console.log(verificaLetras)
+
+
+
+  const verificaIdade = age >= 18 ? true : false;
+  const verificaNome = name.length >= 10 && name.length <= 30 ? true: false;
+  const verificaEmail = email.includes('@' && '.') ? true : false;
+  const verificaEmailVazio = email.length !== 0 ? true : false;
+  const verificaEmailIgual = email === checarEmail ? true : false;
+  // console.log(verificaIdade);
+  // console.log(verificaNome);
+  // console.log(verificaEmail)
+  // console.log(verificaEmailIgual)
+  // console.log(verificaEmailVazio)
+  // console.log(name, email, checarEmail);
+  const sendData = () => {
+
+    // Exercicio 2 verificacao se todos os campos estao corretos !!
+    
+    if (verificaIdade && verificaNome && verificaEmail && verificaEmailIgual && verificaEmailVazio ) {
+      setFormFlow(2)
+    } else alert("Preencha corretamente os campos!")
+    setAge("") 
+    setChecarEmail("")
+    setEmail("")
+    setName("")
+  }
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
-      {formFlow === 1 ? <NameForm
-      // insira aqui suas props
-      /> : <ConfirmationForm />}
-    </MainContainer>
+      {formFlow === 1 ? <NameForm verificaUsuario={sendData} name={name} age={age} email={email} checarEmail={checarEmail}
+        nameChange={onChangeName} emailChange={onChangeEmail} ageChange={onChangeAge} verificaEmail={onChangeEmailVerifica}
+      /> : <ConfirmationForm form={formFlow} form2={setFormFlow}/>}
+    </MainContainer >
   )
 }
 
